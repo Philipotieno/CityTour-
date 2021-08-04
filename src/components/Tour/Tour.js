dcfa55e1c621484b7e0fa397f808a6a79d55b742
philipotieno <iamphilipotieno@gmail.com>
2021-08-04 19:25:19 +0300

    feat: add tour component

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -2,6 +2,29 @@ import React, { Component } from 'react';
 import './tour.scss';
 export default class Tour extends Component {
   render() {
-    return <h1>Hello from tour</h1>;
+    const { id, city, img, name, info } = this.props.tour;
+    const { removeTour } = this.props;
+    return (
+      <article className="tour">
+        <div className="image-container">
+          <img src={img} alt="pic of the tour" />
+          <span className="close-btn">
+            <i className="fas fa-window-close"></i>
+          </span>
+        </div>
+        <div className="tour-info">
+          <h3>{city}</h3>
+          <h4>{name}</h4>
+          <h4>
+            info{''}
+            <span>
+              <i className="fas fa-caret-square-down"></i>
+            </span>
+          </h4>
+          {info}
+          <p></p>
+        </div>
+      </article>
+    );
   }
 }
