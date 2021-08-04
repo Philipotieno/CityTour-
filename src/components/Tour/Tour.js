f9a1c527c44cbaf85cb121d6ddcb27f316b54f6e
philipotieno <iamphilipotieno@gmail.com>
2021-08-04 19:00:15 +0300

    feat: add tourlist component

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,11 +1,7 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import './tour.scss';
 export default class Tour extends Component {
-    render() {
-        return (
-            <h1>
-                Hello from tour
-            </h1>
-        )
-    }
+  render() {
+    return <h1>Hello from tour</h1>;
+  }
 }
--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import Tour from '../Tour';
-
+import './tourlist.scss';
+import { tourData } from '../../tourData';
 export default class TourList extends Component {
+  state = {
+    tours: tourData,
+  };
+  removeTour = (id) => {
+    console.log(id);
+  };
   render() {
+    const { tours } = this.state;
     return (
-      <div>
-        Hello from Tourlist
-        <Tour />
-      </div>
+      <section className="tourlist">
+        {tours.map((tour) => (
+          <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />
+        ))}
+      </section>
     );
   }
 }
