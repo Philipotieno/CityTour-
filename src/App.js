63b3a875bad5fce00c13a2df66f04babd8135b0f
philipotieno <iamphilipotieno@gmail.com>
2021-08-04 18:38:07 +0300

    choe: add project structure

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.scss';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Navbar from './components/Navbar/Navbar';
 import React from 'react';
+import TourList from './components/TourList';
 
 function App() {
   return (
     <React.Fragment>
       <Navbar/>
+      <TourList/>
     </React.Fragment>
   );
 }
---
+++ src/components/Tour/Tour.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Tour extends Component {
+    render() {
+        return (
+            <h1>
+                Hello from tour
+            </h1>
+        )
+    }
+}
