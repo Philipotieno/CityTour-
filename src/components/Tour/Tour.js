9e40bd748ae19320fa7146c2354c001dad563c7b
philipotieno <iamphilipotieno@gmail.com>
2021-08-04 21:29:04 +0300

    feat: add event to handle toggle info

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import './tour.scss';
 export default class Tour extends Component {
+  state = {
+    showInfo: false,
+  };
+
+  handleInfo = () => {
+    this.setState({
+      showInfo: !this.state.showInfo,
+    });
+  };
   render() {
     const { id, city, img, name, info } = this.props.tour;
     const { removeTour } = this.props;
@@ -17,12 +26,11 @@ export default class Tour extends Component {
           <h4>{name}</h4>
           <h5>
             info{''}
-            <span>
+            <span onClick={this.handleInfo}>
               <i className="fas fa-caret-square-down"></i>
             </span>
           </h5>
-          {info}
-          <p></p>
+          {this.state.showInfo && <p>{info}</p>}
         </div>
       </article>
     );
